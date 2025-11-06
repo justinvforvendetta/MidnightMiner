1253366a9d05c950f8ffb1b825b44ab81fe23045
Djeanql <djeanql@tutamail.com>
2025-11-06 22:28:13 +0000

    Add more developer addresses

--- miner.py
+++ miner.py
@@ -28,7 +28,11 @@ except ImportError:
 
 VERSION = "0.3"
 
-DEVELOPER_ADDRESS = random.choice(["addr1v8sd2hwjvumewp3t4rtqz5uwejjv504tus5w279m5k6wkccm0j9gp", "addr1vyel9hlqeft4lwl5shgd28ryes3ejluug0lxhhusnvh2dyc0q92kw", "addr1vxl62mccauqktxyg59ehaskjk75na0pd4utrkvkv822ygsqqt28ph", "addr1vxenv7ucst58q9ju52mw9kjudlwelxnf53kd362jgq8qm5q68uh58", "addr1vylmy9xlwk2u5h5zhp2kwdvznrqgsu54vuc5r9fv8usv4dgdp76wm"])
+DEVELOPER_ADDRESS = random.choice(["addr1v8sd2hwjvumewp3t4rtqz5uwejjv504tus5w279m5k6wkccm0j9gp", "addr1vyel9hlqeft4lwl5shgd28ryes3ejluug0lxhhusnvh2dyc0q92kw", "addr1vxl62mccauqktxyg59ehaskjk75na0pd4utrkvkv822ygsqqt28ph",
+                                   "addr1vxenv7ucst58q9ju52mw9kjudlwelxnf53kd362jgq8qm5q68uh58", "addr1v8hf3d0tgnfn8zp2sgq2gdj9jy4dg6wyzd6uchlvq8n0pnsxp8232", "addr1v8vem45scpapkca8dpgcgdn2wfkg9jva950v8jjh47vrs3qf8sm6z",
+                                   "addr1vyuyd9xxpex2ruzvejeduzknfcn2szyq46qfquxh6n4268qukppmq", "addr1vyrywe247atz5jzu9rspdf7lhvmhd550x45ck7qac295h9s3rs6zd", "addr1v86agy7h3mmphdpyru8tgrjjcpvuuqk8863jspqfd6n60lcxv0xmf",
+                                   "addr1vx5dee9pqnq0r2aypl2ywueqjuvwg0s7dsc7eneyyr3d83g3a08c0", "addr1vx6wfs6z0vrwjutchfhmzk7tazsa09a9ptt8st00nmzshls2npktm", "addr1vx38ypke98t70r4rmkqdtm9c9eqdvjg8ytjc570javaqljcsp0q5h",
+                                   "addr1v8mduamz9a7hghklsuug8szrhm4a0g5j8vxt7zsk2aetw9g8u2ak6","addr1v99tha5x72jdh58rxp3c8amarac6ahf693xwwx4q9hpnnsqcv4nrd"])
 DONATION_RATE = 0.05  # 5%
 
 # Cross-platform file locking
